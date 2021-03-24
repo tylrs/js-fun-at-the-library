75fc22f986c1ee40d80db9832e8942c0fb7915b5
Taylor Galloway <gallowaytaylor@outlook.com>
2021-03-24 17:35:52 -0500

    Add book, warm-up, book-test

--- src/book.js
+++ src/book.js
@@ -1,12 +1,49 @@
-function createTitle() {
-  
-}
+function createTitle(title) {
+  var newTitle = `The ${title}`
+  return newTitle;
+};
 
+function buildMainCharacter(name, age, pronouns) {
+  var mainCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+  return mainCharacter;
+};
+
+function saveReview(reviewText, reviews) {
+  if(reviews.indexOf(reviewText) < 0) {
+    reviews.push(reviewText);
+    return reviews;
+  } else {
+    return reviews;
+  }
+};
+
+function calculatePageCount(bookTitle) {
+  var pageCount = bookTitle.length * 20;
+  return pageCount;
+};
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre
+  }
+  return book;
+};
+function editBook(book) {
+  book.pageCount = book.pageCount * 0.75;
+  return
+};
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
-}
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
+};
