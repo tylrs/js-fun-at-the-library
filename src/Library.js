2ef685997f4c61c84c7c871b00f66f499f2d2478
Taylor Galloway <gallowaytaylor@outlook.com>
2021-03-26 08:51:56 -0500

    Add library and test

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,32 @@
+function createLibrary(name) {
+  var library = {};
+  library.name = name;
+  library.shelves = {};
+  library.shelves.fantasy = [];
+  library.shelves.fiction = [];
+  library.shelves.nonFiction = [];
+  return library;
+};
 
+function addBook(library, book) {
+  for(var shelfGenre in library.shelves) {
+      if (shelfGenre === book.genre) {
+        library.shelves[shelfGenre].unshift(book);
+      }
+    }
+};
 
+function checkoutBook(library, requestedBookTitle, requestedGenreType) {
+    if (library.shelves[requestedGenreType].length === 0) {
+      return `Sorry, there are currently no copies of ${requestedBookTitle} available at the ${library.name}`;
+    } else if (library.shelves[requestedGenreType][0].title === requestedBookTitle) {
+      library.shelves[requestedGenreType].pop();
+      return `You have now checked out ${requestedBookTitle} from the ${library.name}`;
+    };
+
+};
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
